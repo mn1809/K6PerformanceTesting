ac48c9cf4efe8e235e509e6dcd4cc02761d68554
Manoj <manojgowda839@gmail.com>
2026-05-05 18:16:40 +0530

    Final_Push

--- scripts/stress_postlogin_test_copy.js
+++ scripts/stress_postlogin_test_copy.js
@@ -40,11 +40,11 @@ export let options = {
       executor: 'ramping-vus',        // ✅ Gradual ramp instead of instant spike
       startVUs: 0,
       stages: [
-        { duration: '2m',  target: 1  },  // 🔼 Warm up
-        { duration: '2m', target: 1  },  // 🔼 Mid load
-        { duration: '2m', target: 1 },  // 🔼 Peak stress
-        { duration: '2m', target: 1 },  // ➡️ Hold peak
-        { duration: '2m',  target: 0    },  // 🔽 Cool down
+        { duration: '5m',  target: 10  },  // 🔼 Warm up
+        { duration: '10m', target: 50  },  // 🔼 Mid load
+        { duration: '10m', target: 100 },  // 🔼 Peak stress
+        { duration: '10m', target: 100 },  // ➡️ Hold peak
+        { duration: '5m',  target: 0    },  // 🔽 Cool down
       ],
       gracefulRampDown: '30s',
     },
